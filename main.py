b20ce51e445d3a89c0d3d8de5f981782c6455968
kakynchik <ukakyn@gmail.com>
2023-05-25 20:50:20 +0300

    dodatkove

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-"""import urllib.request
-opener = urllib.request.build_opener()
-response = opener.open ("https://httpbin.org/")
-print(response.read())
-print("riznica")"""
-import requests
-response = requests.get("https://httpbin.org/")
-"""print(response.text)
-response_parse = response.text.split("<span>")
-for elem in response_parse:
-    if elem.startswith("$"):
-        print(elem)"""
+import  requests
 from bs4 import BeautifulSoup
+response = requests.get('https://weather.com/uk-UA/weather/'
+                        'today/l/2261b41a35498e667410cb84dfb'
+                        '95d455f977b64d2c02b673df410b7d9ec31ed')
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, features= "html.parser")
-    soup_list = soup.findAll('a', {'href': "/currencies/bitcoin/markets/"})
-    res = soup_list[0].find("span")
-    print(res.text)
+    city = soup.find('h1',{"class":"CurrentConditions--location--1YWj_"})
+    if city:
+        city_name = city.text.strip()
+        weather = weather.text.strip()
+        print(city_name)
+        print(f"temperatura - {weather}")
+    else:
+        print("misto nenaydeno")
+except:
+    print("nema pidklychennia")
